73ed8032d2b2d28c2b15e04fb21de264ac906b8a
Vladimirov Andrei <izotop.natriya@gmail.com>
2015-01-18 16:09:37 +0200

    Добавлен двигающийся мяч и спрайты на будущие объекты.

--- src/MainPackage/MyGame.java
+++ src/MainPackage/MyGame.java
@@ -22,22 +22,47 @@ public class MyGame extends Game
     
     Background background;
     
+    Sprite Ball;
+    double speed = 0.3;
+    SpriteGroup BALLS;
+    
     @Override
     public void initResources()
     {
          background = new ColorBackground(Color.DARK_GRAY, ScreenX, ScreenY);
+         
+         BALLS = new SpriteGroup("Balls group");
+         Ball = new Sprite(getImage("Images/Ball_standart.png"), ScreenX/2, ScreenY/2);
+         Ball.setSpeed(speed, speed);
+         Ball.setImmutable(true);
+         Ball.setID(2);
+         BALLS.add(Ball);
     }
     
     @Override
     public void update(long elapsedTime)
     {
         background.update(elapsedTime); 
+        
+        BALLS.update(elapsedTime);
+        Sprite[] sprite;
+        sprite = BALLS.getSprites();
+        BALLS.clear();
+        for (int i = 0; i < sprite.length - 1; i++)
+        {
+            if (sprite[i] != null)
+            {
+                sprite[i].setID(2);
+                BALLS.add(sprite[i]);
+            }
+        }
     }
     
     @Override
     public void render(Graphics2D g)
     {
          background.render(g);
+         BALLS.render(g);
     }
     
     public static void main(String[] args)
